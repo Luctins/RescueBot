8bc959ea74a4b1f59561e9f3bb9da1b9a65b4a29
mendes110898 <lucas.martinsmendes@gmail.com>
2017-06-09 00:54:11 -0300

    control scheme mostly ready

---
+++ robot/src/robot.h
@@ -0,0 +1,106 @@
+/*------------------------------
+	Robot header
+
+	Header Containing:
+		Defines,
+		Macros
+
+	Created By: Lucas Martins Mendes
+	Date: 18/02/2017 12:45
+
+------------------------------*/
+
+//TODO: Define pins
+
+#ifndef ROBOT_H
+#define ROBOT_H
+
+//Defines---------------
+#define 	BOARD_MODEL 	"Arduino Uno"
+
+#define   STRT_BTN   13  //Start Button PIN
+
+//Serial
+#define 	SERIAL_SPEED	9600
+//#define   BT_SERIAL_SPEED 9600
+
+//Sensor ID's
+#define 	LGT_SENSOR		1
+#define 	DST_SENSOR		2
+
+//Light/color sensors
+#define 	SNSR_LGT_CTR 	8
+#define 	SNSR_LGT_LFT	6
+#define 	SNSR_LGT_RGT	4
+#define   SNSR_LGT_FRNT  4
+
+//Controller
+//#define   CNTR_GAIN     1     //controller gain
+#define   CNTR_STPT     511  //controller default SetPoint
+
+//Modes
+#define 	MODE_IDLE		0	  //Stand by, sensors disabled/not beiyng read
+#define 	MODE_AUTO		1  	//Autonomous
+//#define 	MODE_REMOTE		2	//Remote controlled by other application
+
+//Control Defines,
+  /* comment these for removing/altering
+  specific parts of the program related to it */
+
+//DO NOT DEFINE BOTH OF THOSE, ALWAYS KEEP ONLY ONE ACTIVE
+//#define 	HBRIDGE 1 //Minha ponte H bridge type
+#define HBRIDGE_2 1 //L293 bridge type
+
+//Peripherals
+//#define 	RGB_LED
+//#define 	GYRO
+
+#ifdef HBRIDGE_2
+#ifdef HBRIDGE
+#error "Do not define 2 Hbridge's at the same time"
+#endif
+#endif
+
+#ifndef HBRIDGE_2
+#ifndef HBRIDGE
+#error "Please define one Hbridge"
+#endif
+#endif
+
+//Pins
+
+//H bridge, 1 is forward, 2 is reverse
+#ifdef HBRIDGE_2
+
+#define 	ENG_RGT_1		1
+#define 	ENG_RGT_2		2
+#define 	ENG_RGT_EN	3
+
+#define 	ENG_LFT_1		4
+#define 	ENG_LFT_2		5
+#define 	ENG_LFT_EN	6
+
+#endif
+
+#ifdef HBRIDGE
+
+#define 	ENG_RGT_1		3
+#define 	ENG_RGT_2		5
+
+#define 	ENG_LFT_1		7
+#define 	ENG_LFT_2		9
+
+#endif
+
+//Led RGB
+#ifdef 		RGB_LED
+#define 	RGB_LED_PIN
+#endif
+
+//GYRO
+#ifdef 		GYRO
+#define 	GYRO_PIN
+#endif
+
+//-----------------------------------------------
+#endif /* ROBOT_H */
--- robot/src/robot.ino
+++ robot/src/robot.ino
@@ -3,7 +3,10 @@
 
 	For: Arduino UNO
 
-	Created By: Lucas Martins Mendes
+	Created By:
+		Jaqueline Ribeiro,
+		Lucas Martins Mendes
+
 	Date: 18/02/2017 11:55
 
 ------------------------------*/
@@ -13,6 +16,10 @@
 
 //Project Defines
 #include "robot.h"
+
+//-----------------------------
+// Object declaration
+//-----------------------------
 
 class Robot {
 	public:
@@ -27,13 +34,15 @@ class Robot {
 			Serial.begin(SERIAL_SPEED);
 
 			//Pin setup
+			pinMode(STRT_BTN, INPUT);
 			pinMode(ENG_RGT_1, OUTPUT);
 			pinMode(ENG_RGT_2, OUTPUT);
 			pinMode(ENG_LFT_1, OUTPUT);
 			pinMode(ENG_LFT_2, OUTPUT);
 
+
 			//if using the protoboard H bridge
-			#ifdef NO_HBRIDGE
+			#ifdef HBRIDGE_2
 			pinMode(ENG_RGT_EN, OUTPUT);
 			pinMode(ENG_LFT_EN, OUTPUT);
 			#endif
@@ -41,11 +50,11 @@ class Robot {
 			//Sensors
 			//TODO: Set up sensors here
 
-
 			setMode(MODE_IDLE);
 
 			debug(F("done"));
 		}
+
 
 		#ifdef HBRIDGE
 		void tankDrive (int left, int right) {
@@ -60,7 +69,7 @@ class Robot {
 			debug(F("tried to move idle"));
 		}
 
-		#elif NO_HBRIDGE
+		#elif HBRIDGE_2
 		void tankDrive (int left, int right) {
 			//Tank type steering
 			//negative values go backwards
@@ -69,8 +78,7 @@ class Robot {
 				setEngine(left, ENG_LFT_EN);
 				setEngine(right, ENG_RGT_EN);
 			}
-			else if(mode == MODE_IDLE)
-				debug(F("tried to move idle"));
+			else if(mode == MODE_IDLE) { debug(F("tried to move idle")); }
 		}
 		#endif
 
@@ -80,13 +88,11 @@ class Robot {
 			the left one is zero,
 			the middle one is 512 and,
 			the right one is 1204. */
-			int sensorValue;
+			int sensorValue = 0; //Placeholder
 			//TODO: read sensors here
-
 
 			return sensorValue;
 		}
-
 
 		void setMode(uint8_t modeFlag) {
 			mode = modeFlag;
@@ -94,6 +100,7 @@ class Robot {
 			debug(modeFlag);
 		}
 
+		//
 		template <typename Generic>
 		void debug(Generic text) {
 			Serial.println(text);
@@ -120,7 +127,7 @@ class Robot {
 			}
 
 		}
-		#elif NO_HBRIDGE
+		#elif HBRIDGE_2
 		void setEngine(int value, uint8_t engine) {
 			if(value>0) {
 				analogWrite(engine, value);
@@ -137,21 +144,54 @@ class Robot {
 
 } robot;
 
+/*
+//Classe de controle
+class Controller {
+	public:
 
-//-----------------------------
-// Object declaration
-//-----------------------------
+	//Basicamente uma função "init" da classe
+  Controller(int _kP, int _setpoint) {
+    kP = _kP;
+		setPoint = _setpoint;
+  }
 
+//Adiciona nova amostra
+  void addNewSample(double _sample){
+    sample = _sample;
+  }
 
+  int compute() {
+    // Implementação PID
+    error = setPoint - sample; //Calcula erro
+    //float deltaTime = (millis() - lastProcess) / 1000.0; //computa delta de tempo
+    //lastProcess = millis();
+    P = error * kP;
+    return P;
+  }
+	private:
+		int error;
+		int sample; //Amostra
+		int kP; //Ganho do controlador
+		float P; //valor de saída do controlador
+
+		int setPoint; //ponto objetivo
+		//long lastProcess;
+		//Quanto tempo passou
+		//desde a ultima vez que o sistema computou os valores
+};
+
+Controller controller(CNTR_GAIN, CNTR_STPT);
+*/
 
 //------------------------------
 // ---------- Setup -----------
+
 void setup()
 {
 	//Hardware init
 	robot.init();
 	robot.setMode(MODE_IDLE);
-
+	//int setpoint = CNTR_STPT;
 }
 
 
@@ -160,12 +200,50 @@ void setup()
 
 void loop()
 {
-//TODO: Bot start switch with debounce
+	//namespace robot;
+	int right;
+	int left;
+	int center;
+	int victim;
 
+	switch(robot.mode) {
+		case MODE_AUTO:
+			//controller.addNewSample(robot.computeSensorAvg());
+			//int compensation = controller.compute();
+			right = robot.readSensors(LGT_SENSOR,SNSR_LGT_RGT);
+ 			left = robot.readSensors(LGT_SENSOR,SNSR_LGT_LFT);
+ 			center = robot.readSensors(LGT_SENSOR,SNSR_LGT_CTR);
+			//center /= 1.5;
+			robot.tankDrive(center+right,center+left); //depois explico exatamente como isso funciona
+			//             |
+			//             |
+			//             |
+			//             |
+			//------------------------------
+			//
 
+			victim = robot.readSensors(LGT_SENSOR,SNSR_LGT_FRNT);
+
+		break;
+
+		case MODE_IDLE :
+			/*
+			if (millis() % 1001 == 0) {
+				robot.debug(F("Waiting"));
+			}
+			*/
+			if(digitalRead(STRT_BTN) == 1) {
+				delay(50); //Button Debounce
+				while (digitalRead(STRT_BTN) == 1); //wait for button to be released
+				robot.setMode(MODE_AUTO);
+				}
+		break;
+
+		default:
+			robot.debug(F("What?, How did you end up in here?"));
+		break;
+	}
 }
-
-
 /*----------
 	EOF
 ----------*/
